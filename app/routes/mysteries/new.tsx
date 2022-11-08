026d59cb21a59fb4bbda7e690e76f93f7d372e9c
Terone <bterone@gmail.com>
2022-11-08 17:52:05 +0700

    [gh-5] Add mystery form

--- app/routes/mysteries/new.tsx
+++ app/routes/mysteries/new.tsx
@@ -1,3 +1,105 @@
+import { Form } from "@remix-run/react";
+import { useRef } from "react";
+
 export default function MysteryNewPage() {
-  return <h1>Mystery New Page</h1>;
+  const titleRef = useRef<HTMLInputElement>(null);
+  const conceptRef = useRef<HTMLTextAreaElement>(null);
+  const hookRef = useRef<HTMLTextAreaElement>(null);
+  const dayRef = useRef<HTMLInputElement>(null);
+  const shadowsRef = useRef<HTMLInputElement>(null);
+  const duskRef = useRef<HTMLInputElement>(null);
+  const nightfallRef = useRef<HTMLInputElement>(null);
+  const midnightRef = useRef<HTMLInputElement>(null);
+
+  return (
+    <Form method="post">
+      <div className="mb-6">
+        <label className="flex flex-col gap-1">
+          <span>Title: </span>
+          <input
+            ref={titleRef}
+            name="title"
+            className="flex-1 rounded-md border-2 px-3 text-lg leading-loose"
+          />
+        </label>
+      </div>
+
+      <div className="mb-6">
+        <label className="flex flex-col gap-1">
+          <span>Concept: </span>
+          <textarea
+            ref={conceptRef}
+            name="concept"
+            className="flex-1 rounded-md border-2 px-3 text-lg leading-loose"
+          />
+        </label>
+      </div>
+
+      <div className="mb-6">
+        <label className="flex flex-col gap-1">
+          <span>Hook: </span>
+          <textarea
+            ref={hookRef}
+            name="hook"
+            className="flex-1 rounded-md border-2 px-3 text-lg leading-loose"
+          />
+        </label>
+      </div>
+
+      <div className="mb-6">
+        <label className="flex flex-col gap-1">
+          <span>Day: </span>
+          <input
+            ref={dayRef}
+            name="day"
+            className="flex-1 rounded-md border-2 px-3 text-lg leading-loose"
+          />
+        </label>
+      </div>
+
+      <div className="mb-6">
+        <label className="flex flex-col gap-1">
+          <span>Shadows: </span>
+          <input
+            ref={shadowsRef}
+            name="shadows"
+            className="flex-1 rounded-md border-2 px-3 text-lg leading-loose"
+          />
+        </label>
+      </div>
+
+      <div className="mb-6">
+        <label className="flex flex-col gap-1">
+          <span>Dusk: </span>
+          <input
+            ref={duskRef}
+            name="dusk"
+            className="flex-1 rounded-md border-2 px-3 text-lg leading-loose"
+          />
+        </label>
+      </div>
+
+      <div className="mb-6">
+        <label className="flex flex-col gap-1">
+          <span>Nightfall: </span>
+          <input
+            ref={nightfallRef}
+            name="nightfall"
+            className="flex-1 rounded-md border-2 px-3 text-lg leading-loose"
+          />
+        </label>
+      </div>
+
+      <div className="mb-6">
+        <label className="flex flex-col gap-1">
+          <span>Midnight: </span>
+          <input
+            ref={midnightRef}
+            name="midnight"
+            className="flex-1 rounded-md border-2 px-3 text-lg leading-loose"
+          />
+        </label>
+      </div>
+    </Form>
+  );
 }
---
+++ prisma/migrations/20221108104904_update_mystery_information/migration.sql
@@ -0,0 +1,10 @@
+-- AlterTable
+ALTER TABLE "Mystery" ADD COLUMN "concept" TEXT;
+ALTER TABLE "Mystery" ADD COLUMN "day" TEXT;
+ALTER TABLE "Mystery" ADD COLUMN "dusk" TEXT;
+ALTER TABLE "Mystery" ADD COLUMN "hook" TEXT;
+ALTER TABLE "Mystery" ADD COLUMN "midnight" TEXT;
+ALTER TABLE "Mystery" ADD COLUMN "nightfall" TEXT;
+ALTER TABLE "Mystery" ADD COLUMN "notes" TEXT;
+ALTER TABLE "Mystery" ADD COLUMN "secrets" TEXT;
+ALTER TABLE "Mystery" ADD COLUMN "shadows" TEXT;
